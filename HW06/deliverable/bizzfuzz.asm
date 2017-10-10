e2257d590e59e08b0cfce3ace2a494f1bf20c8fc
Your Name <wjbjimmy@gmail.com>
2017-10-09 19:41:56 -0700

    deliveredhw6

---
+++ HW06/deliverable/bizzfuzz.asm
@@ -0,0 +1,72 @@
+;;==============================
+;; Name: Jingbo Wang
+;;===============================
+;; Write a program to do the following.
+;;
+;; Given an array of positive integers (`ARRAY`) of length `LENGTH`,
+;; for each integer `n` inside the array:
+;;      Print 'Bizz' and a newline if `n` is divisible by 3 and not by 5 
+;;          (`n mod 3 == 0` and `n mod 5 != 0`)
+;;      Print 'Fuzz' and a newline if `n` is divisible by 5 and not by 3
+;;          (`n mod 3 != 0` and `n mod 5 == 0`)
+;;      Print 'BizzFuzz' and a newline if `n` is divisible by 3 and 5.
+;;          (`n mod 3 == 0` and `n mod 5 == 0`)
+;;
+;; Reminder that the newline character is '\n'. Only print newlines in the
+;; iterations where you print something. (i.e. If `n` is 7, don't print
+;; anything, but if `n` is 15, print 'BizzFuzz\n'.) 
+
+.orig x3000
+	LD R1, ARRAY ;put the first index of array in R1
+	LD R2, LENGTH ;put the length in R2
+	BRz FINISH ;if array is empty, don't do anything
+START	AND R5, R5, #0 ;R5 is the status
+	LDR R3, R1, #0 ;put element in array in R3
+	ADD R4, R3, #0 ;make a copy to R4
+	BRz BF ;if value is zero, print
+REPEAT3	ADD R3, R3, #-3
+	BRp REPEAT3 ;keep decrement value by 3
+	BRz THREE ;the value is divisible by 3
+	BRn REPEAT5 ;not divisible by 3, try 5
+THREE   ADD R5, R5, #1 ;R5==1 means it is divisible by 3
+REPEAT5 ADD R4, R4, #-5
+	BRp REPEAT5 ;keep decrement value by 5
+	BRz FIVE ;the value is divisible by 5
+	BRn NOTFIVE ;not divisible by 5, go to final check
+FIVE	ADD R5, R5, #-1 ;R5==-1 means it is divisible by 5
+	BRz BF ;if status is 0, print BIZZFUZZ
+ABF	BRn F ;if status if negative, print FUZZ
+AF	BRnzp FINISH; finish checking
+NOTFIVE ADD R5, R5, #0 ;get status
+	BRp B ;if status is positive, print BIZZ
+AB	BRnzp FINISH; finish checking
+FINISH	ADD R1, R1, #1 ;point to the next element
+	ADD R2, R2, #-1 ;keep track of counter
+	BRp START ;keep looping if there are still more to check
+	HALT
+BF  	LEA R0, BIZZFUZZ
+	PUTS
+	BRnzp ABF
+F	LEA R0, FUZZ
+	PUTS
+	BRnzp AF
+B	LEA R0, BIZZ
+	PUTS
+	BRnzp AB
+ARRAY       .fill x6000
+LENGTH      .fill 5
+BIZZ	    .stringz "Bizz\n"
+FUZZ	    .stringz "Fuzz\n"
+BIZZFUZZ    .stringz "BizzFuzz\n"
+
+.end
+
+;; Sample `ARRAY` for manual testing. The tester will rewrite this and the
+;; `LENGTH`.
+.orig x6000
+.fill 4
+.fill 5
+.fill 15
+.fill 0
+.fill 3
+.end
---
+++ HW06/deliverable/insert.asm
@@ -0,0 +1,49 @@
+;;==============================
+;; Name: Jingbo Wang
+;;===============================
+;; Write a program to add an element at a certain index in an array.
+;; For the tester, it checks (element, index, [array]) 
+;; Note 1) Do not delete any elements from the array, just shift elements when adding
+;;      2) Elements can be inserted at the front, middle, or back of the array
+;;      3) The array begins at index 0
+
+.orig x3000
+	LD R0, INDEX ;put the index in R0
+	LD R1, ARRAY ;put the array head in R1
+	ADD R0, R0, R1 ;the insert index in R0
+	LD R2, LENGTH ;put length in R2
+	ADD R1, R1, R2
+	ADD R1, R1, #-1 ;put the last index of array in R1 (the source of the first shifting)
+	LD R3, INDEX ;put INDEX in R3, cc = p
+	BRz SHIFT ;if index == 0; start shifting
+SETUP	ADD R2, R2, #-1 ;setup the counter for shifting
+	ADD R3, R3, #-1 ;
+	BRp SETUP ;R2 now stores the number of elements I need to shift
+SHIFT	ADD R3, R1, #1 ;the destination of shifting
+	LDR R4, R1, #0 ;put the shifting element in R4
+	STR R4, R3, #0 ;put the shifting elemtn back to memory shifted
+	ADD R1, R1, #-1
+	ADD R3, R3, #-1
+	ADD R2, R2, #-1 ;get to the next element
+	BRp SHIFT ;shift till the insert index is open
+	LD R1, NUMBER ;R1 holds the value to be insert
+	STR R1, R0, #0 ;store
+    HALT
+ARRAY   .fill x6000
+LENGTH  .fill 10
+NUMBER  .fill 15
+INDEX   .fill 3
+.end
+
+.orig x6000
+.fill 2
+.fill 9
+.fill 6
+.fill 7
+.fill 8
+.fill 4
+.fill 10
+.fill 11
+.fill 3
+.fill 14
+.end
