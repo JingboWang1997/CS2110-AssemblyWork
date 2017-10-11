f535015daa90184d16ac57c6d4ad11a6fdef6dd5
JingboWang1997 <wjbjimmy@gmail.com>
2017-10-11 16:37:19 -0400

    lab9

---
+++ Lab 9/sumtorial.asm
@@ -0,0 +1,29 @@
+;;======================================================================
+;; Assignment: Lab 09
+;; Description: An assembly program to calculate sumtorial
+;;======================================================================
+
+;;Pseudocode:
+;;-----------
+
+; Sumtorial(n)
+; {
+;     int i;
+;     int sum = 0;
+;     for(i = 0; i < n; i++)
+;     {
+;        sum += n
+;        n = n-1;
+;     }
+;     return sum;
+; }
+;;-----------
+
+.orig x3000
+
+;;Your Code Here
+
+HALT
+
+NUM     .FILL     #5
+.end
