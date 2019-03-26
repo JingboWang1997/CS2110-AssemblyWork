4cc2873eb65eb5f20800bbff3ec90d78f4f5dadb
JingboWang1997 <wjbjimmy@gmail.com>
2019-03-25 20:26:42 -0400

    final update

--- Lab 17/sizeof.c
+++ Lab 17/sizeof.c
@@ -1,16 +1,44 @@
 #include <stdio.h>
 
-struct p {char a; char b;};
-
-int main(int argc, char const *argv[])
+typedef struct
 {
-	printf("char: %lu\n", sizeof(char));
-	printf("short: %lu\n", sizeof(short));
-	printf("int: %lu\n", sizeof(int));
-	printf("long: %lu\n", sizeof(long));
-	printf("int*: %lu\n", sizeof(int*));
-	printf("char*: %lu\n", sizeof(char*));
-	printf("struct{char, char}: %lu\n", sizeof(struct{char a; char b;}));
-	printf("struct{short, char, int}: %lu\n", sizeof(struct{short p; char a; int b;}));
+    int x;
+    char* y;
+} something;
+
+
+// Examine this function that copies this structure into the dest parameter
+
+
+void copy_struct(const something* src, something* dest)
+{
+    char *pdest = (char *)dest->y;
+    char *psrc = (char *)src->y;
+
+    for (int i = 0; i < sizeof(*(src->y)); i++)
+    {
+    	pdest[i] = psrc[i];
+    }
+}
+
+int main()
+{
+    char s[6];
+    something a, b;
+    a.x = 32;
+    a.y = s;
+    a.y[0] = 'H';
+    a.y[1] = 'E';
+    a.y[2] = 'L';
+    a.y[3] = 'L';
+    a.y[4] = 'O';
+    a.y[5] = '\0';
+
+
+    copy_struct(&a, &b);
+    printf("a.x = %d, a.y = %s, b.x = %d, b.y = %s\n", a.x, a.y, b.x, b.y);
+    a.x = 27;
+    a.y[0] = 'J';
+    printf("a.x = %d, a.y = %s, b.x = %d, b.y = %s\n", a.x, a.y, b.x, b.y);
 
 }
